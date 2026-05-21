a527e73afea0049580e063509201ed5055ac383d
David Souther <david.souther@nominal.io>
2026-05-21 11:24:32 -0400

    Link installs brew if necessary

--- link.sh
+++ link.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+[ -d /opt/homebrew/bin ] || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+[ -x /opt/homebrew/bin/brew ] || eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install stow
+
 stow -t $HOME -R sh vim zsh vscode
+mkdir -p $HOME/.claude
+stow -t $HOME/.claude claude
 
 git clone https://github.com/VundleVim/Vundle.vim.git \
   ~/.vim/bundle/Vundle.vim >/dev/null 2>&1
